78619041515e49a624bbdd7552a0f43ba01beb97
indraagstin025 <indraagustin012@gmail.com>
2026-09-07 20:53:31 +0700

    feat(views): implement UI template resources/views/components/icon-editor.blade.php

--- resources/views/components/icon-editor.blade.php
+++ resources/views/components/icon-editor.blade.php
@@ -13,7 +13,13 @@
     $iconFile = $iconMap[$ed] ?? 'vscode.svg';
 @endphp
 
-<img src="{{ asset('assets/icons/' . $iconFile) }}" 
-     alt="{{ $editor }}" 
-     loading="lazy"
-     {{ $attributes->merge(['class' => 'w-3.5 h-3.5 shrink-0 object-contain inline-block']) }} />
+@if($ed === 'terminal')
+    <svg {{ $attributes->merge(['class' => 'w-3.5 h-3.5 shrink-0 inline-block text-current']) }} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 17l6-6-6-6m8 14h8"></path>
+    </svg>
+@else
+    <img src="{{ asset('assets/icons/' . $iconFile) }}" 
+         alt="{{ $editor }}" 
+         loading="lazy"
+         {{ $attributes->merge(['class' => 'w-3.5 h-3.5 shrink-0 object-contain inline-block']) }} />
+@endif
